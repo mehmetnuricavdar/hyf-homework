317cf058a3f8cc82b21c476b9d8db6712596e068
mehmetnuricavdar <mn.cavdar21@gmail.com>
2022-10-27 11:47:32 +0200

    addStudentToClass function

--- javascript/javascript1/week2/script.js
+++ javascript/javascript1/week2/script.js
@@ -82,6 +82,35 @@ function whatToWear(temperature) {
   }
 }
 
-
 // Student manager
 
+const class23Students = [];
+function addStudentToClass(studentName) {
+  // You write code here
+  //   for (let i = 0; i < class23Students.length; i++) {
+  //     if (studentName === class23Students[i]) {
+  //       return `Student ${studentName} is already in the class`;
+  //     }
+  //   }
+  if (class23Students.includes(studentName)) {
+    return `Student ${studentName} is already in the class`;
+  }
+  if (class23Students.length >= 6) {
+    if (studentName === "Queen") {
+      return class23Students.push(studentName);
+    } else {
+      return `Cannot add more students to class 23`;
+    }
+  }
+
+  if (studentName === "") {
+    return `You cannot add an empty name!`;
+  }
+
+  return class23Students.push(studentName);
+}
+
+function getNumberOfStudents() {
+  // You write code here
+  return class23Students.length;
+}
