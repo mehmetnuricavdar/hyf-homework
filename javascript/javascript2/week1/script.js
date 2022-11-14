5e66cd48ea8172199341e227533ac1d1db15b921
mehmetnuricavdar <mn.cavdar21@gmail.com>
2022-11-15 00:24:26 +0100

    js2 w1 hw- spiritial animal and danish letters

--- javascript/javascript2/week1/script.js
+++ javascript/javascript2/week1/script.js
@@ -8,3 +8,286 @@ for (let i = 0; i < danishWords.length; i++) {
   }
 }
 console.log(minWord);
+
+// ######## 1.2. Find and count the Danish letters
+const danishLetters = ["å", "æ", "ø"];
+const danishString = "Jeg har eåøn blåæ bil";
+
+function findingDanishLetter() {
+  let total = 0;
+  let letterArray = danishString.split("");
+  let newArray = letterArray.filter((x) => danishLetters.includes(x));
+  const counts = {};
+  newArray.forEach(function (x) {
+    counts[x] = (counts[x] || 0) + 1;
+  });
+  console.log(counts);
+}
+
+// ######## 2. Spirit animal name generator
+
+const animals = [
+  "Aardvark",
+  "Albatross",
+  "Alligator",
+  "Alpaca",
+  "Ant",
+  "Anteater",
+  "Antelope",
+  "Ape",
+  "Armadillo",
+  "Donkey",
+  "Baboon",
+  "Badger",
+  "Barracuda",
+  "Bat",
+  "Bear",
+  "Beaver",
+  "Bee",
+  "Bison",
+  "Boar",
+  "Buffalo",
+  "Butterfly",
+  "Camel",
+  "Capybara",
+  "Caribou",
+  "Cassowary",
+  "Cat",
+  "Caterpillar",
+  "Cattle",
+  "Chamois",
+  "Cheetah",
+  "Chicken",
+  "Chimpanzee",
+  "Chinchilla",
+  "Chough",
+  "Clam",
+  "Cobra",
+  "Cockroach",
+  "Cod",
+  "Cormorant",
+  "Coyote",
+  "Crab",
+  "Crane",
+  "Crocodile",
+  "Crow",
+  "Curlew",
+  "Deer",
+  "Dinosaur",
+  "Dog",
+  "Dogfish",
+  "Dolphin",
+  "Dotterel",
+  "Dove",
+  "Dragonfly",
+  "Duck",
+  "Dugong",
+  "Dunlin",
+  "Eagle",
+  "Echidna",
+  "Eel",
+  "Eland",
+  "Elephant",
+  "Elk",
+  "Emu",
+  "Falcon",
+  "Ferret",
+  "Finch",
+  "Fish",
+  "Flamingo",
+  "Fly",
+  "Fox",
+  "Frog",
+  "Gaur",
+  "Gazelle",
+  "Gerbil",
+  "Giraffe",
+  "Gnat",
+  "Gnu",
+  "Goat",
+  "Goldfinch",
+  "Goldfish",
+  "Goose",
+  "Gorilla",
+  "Goshawk",
+  "Grasshopper",
+  "Grouse",
+  "Guanaco",
+  "Gull",
+  "Hamster",
+  "Hare",
+  "Hawk",
+  "Hedgehog",
+  "Heron",
+  "Herring",
+  "Hippopotamus",
+  "Hornet",
+  "Horse",
+  "Human",
+  "Hummingbird",
+  "Hyena",
+  "Ibex",
+  "Ibis",
+  "Jackal",
+  "Jaguar",
+  "Jay",
+  "Jellyfish",
+  "Kangaroo",
+  "Kingfisher",
+  "Koala",
+  "Kookabura",
+  "Kouprey",
+  "Kudu",
+  "Lapwing",
+  "Lark",
+  "Lemur",
+  "Leopard",
+  "Lion",
+  "Llama",
+  "Lobster",
+  "Locust",
+  "Loris",
+  "Louse",
+  "Lyrebird",
+  "Magpie",
+  "Mallard",
+  "Manatee",
+  "Mandrill",
+  "Mantis",
+  "Marten",
+  "Meerkat",
+  "Mink",
+  "Mole",
+  "Mongoose",
+  "Monkey",
+  "Moose",
+  "Mosquito",
+  "Mouse",
+  "Mule",
+  "Narwhal",
+  "Newt",
+  "Nightingale",
+  "Octopus",
+  "Okapi",
+  "Opossum",
+  "Oryx",
+  "Ostrich",
+  "Otter",
+  "Owl",
+  "Oyster",
+  "Panther",
+  "Parrot",
+  "Partridge",
+  "Peafowl",
+  "Pelican",
+  "Penguin",
+  "Pheasant",
+  "Pig",
+  "Pigeon",
+  "Pony",
+  "Porcupine",
+  "Porpoise",
+  "Quail",
+  "Quelea",
+  "Quetzal",
+  "Rabbit",
+  "Raccoon",
+  "Rail",
+  "Ram",
+  "Rat",
+  "Raven",
+  "Red deer",
+  "Red panda",
+  "Reindeer",
+  "Rhinoceros",
+  "Rook",
+  "Salamander",
+  "Salmon",
+  "Sand Dollar",
+  "Sandpiper",
+  "Sardine",
+  "Scorpion",
+  "Seahorse",
+  "Seal",
+  "Shark",
+  "Sheep",
+  "Shrew",
+  "Skunk",
+  "Snail",
+  "Snake",
+  "Sparrow",
+  "Spider",
+  "Spoonbill",
+  "Squid",
+  "Squirrel",
+  "Starling",
+  "Stingray",
+  "Stinkbug",
+  "Stork",
+  "Swallow",
+  "Swan",
+  "Tapir",
+  "Tarsier",
+  "Termite",
+  "Tiger",
+  "Toad",
+  "Trout",
+  "Turkey",
+  "Turtle",
+  "Viper",
+  "Vulture",
+  "Wallaby",
+  "Walrus",
+  "Wasp",
+  "Weasel",
+  "Whale",
+  "Wildcat",
+  "Wolf",
+  "Wolverine",
+  "Wombat",
+  "Woodcock",
+  "Woodpecker",
+  "Worm",
+  "Wren",
+  "Yak",
+  "Zebra",
+];
+
+// creating input area for name
+const myInput = document.createElement("input");
+myInput.setAttribute("id", "textArea");
+myInput.setAttribute("placeholder", "Enter your name");
+document.querySelector(".myDiv").appendChild(myInput);
+
+// creating button for call the function
+const myButton = document.createElement("button");
+myButton.setAttribute("type", "submit");
+myButton.innerHTML = "Find Your Spirit Animal!";
+document.querySelector(".myDiv").appendChild(myButton);
+
+//creating function for generating spirit animals
+function spiritAnimalGenerator() {
+  const inputValue = document.querySelector("#textArea").value;
+  const spiritAnimal = animals[Math.floor(Math.random() * 223)];
+  if (inputValue !== "") {
+    const text = document.createElement("p");
+    text.innerHTML = `Hi ${inputValue}, your spirit animal is ${spiritAnimal}`;
+    document.body.appendChild(text);
+  } else {
+    alert("Please enter your name!");
+  }
+}
+
+// creating event listener with "if" conditional
+
+myButton.addEventListener("click", function () {
+  if (document.querySelector("#clicked").checked) {
+    spiritAnimalGenerator();
+  }
+});
+
+myInput.addEventListener("mouseover", function () {
+  if (document.querySelector("#onHover").checked) {
+    spiritAnimalGenerator();
+  }
+});
