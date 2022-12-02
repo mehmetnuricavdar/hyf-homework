9f77790da1a5f55fcce8319e2fbe02a2a0141988
mehmetnuricavdar <mn.cavdar21@gmail.com>
2022-12-02 16:01:35 +0100

    game exercise

--- javascript/javascript2/week3/script.js
+++ javascript/javascript2/week3/script.js
@@ -1,136 +1,212 @@
-// ########  Warmups
-// ######## 1.1
-const text = () => {
-  console.log("Called after 2.5 seconds");
-};
-
-setTimeout(text, 2500);
-
-// ####### 1.2
-const hw2 = (delay, stringToLog) => {
+(function () {
+  "use strict";
+  // ########  Warmups
+  // ######## 1.1
   const text = () => {
-    console.log(stringToLog);
+    console.log("Called after 2.5 seconds");
   };
 
-  setTimeout(text, delay);
-};
+  setTimeout(text, 2500);
 
-hw2(5000, "Hello World!");
+  // ####### 1.2
+  const hw2 = (delay, stringToLog) => {
+    const text = () => {
+      console.log(stringToLog);
+    };
 
-// ######## 1.3
-const clickButton = document.querySelector("#click");
-clickButton.addEventListener("click", () => {
-  hw2(5000, "Called after 5 seconds");
-});
+    setTimeout(text, delay);
+  };
 
-// ######### 1.4
+  hw2(5000, "Hello World!");
 
-const firstFunction = () => console.log("Earth");
-const secondFunction = () => console.log("Saturn");
-const thirdFunction = (planetLogFunction) => {
-  if (typeof planetLogFunction === "function") {
-    if (planetLogFunction === firstFunction) {
-      return firstFunction();
-    } else if (planetLogFunction === secondFunction) {
-      return secondFunction();
+  // ######## 1.3
+  const clickButton = document.querySelector("#click");
+  clickButton.addEventListener("click", () => {
+    hw2(5000, "Called after 5 seconds");
+  });
+
+  // ######### 1.4
+
+  const firstFunction = () => console.log("Earth");
+  const secondFunction = () => console.log("Saturn");
+  const thirdFunction = (planetLogFunction) => {
+    if (typeof planetLogFunction === "function") {
+      if (planetLogFunction === firstFunction) {
+        return firstFunction();
+      } else if (planetLogFunction === secondFunction) {
+        return secondFunction();
+      }
+    }
+  };
+  thirdFunction(firstFunction);
+  thirdFunction(secondFunction);
+
+  // ########## 1.5 "Log location"
+  const logButton = document.querySelector("#find-me");
+  function geoFindMe() {
+    const status = document.querySelector("#status");
+    const mapLink = document.querySelector("#map-link");
+
+    mapLink.href = "";
+    mapLink.textContent = "";
+
+    function success(position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      status.textContent = "";
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+
+    function error() {
+      status.textContent = "Unable to retrieve your location";
+    }
+
+    if (!navigator.geolocation) {
+      status.textContent = "Geolocation is not supported by your browser";
+    } else {
+      status.textContent = "Locating…";
+      navigator.geolocation.getCurrentPosition(success, error);
     }
   }
-};
-thirdFunction(firstFunction);
-thirdFunction(secondFunction);
 
-// ########## 1.5 "Log location"
-const logButton = document.querySelector("#find-me");
-function geoFindMe() {
-  const status = document.querySelector("#status");
-  const mapLink = document.querySelector("#map-link");
+  logButton.addEventListener("click", geoFindMe);
 
-  mapLink.href = "";
-  mapLink.textContent = "";
+  // ########### 1.6 Google maps
+  let map;
 
-  function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    status.textContent = "";
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
   }
 
-  function error() {
-    status.textContent = "Unable to retrieve your location";
-  }
+  window.initMap = initMap;
 
-  if (!navigator.geolocation) {
-    status.textContent = "Geolocation is not supported by your browser";
-  } else {
-    status.textContent = "Locating…";
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-}
+  // #### 1.7
 
-logButton.addEventListener("click", geoFindMe);
-
-// ########### 1.6 Google maps
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  // #### 1.8
+  window.addEventListener("dblclick", (e) => {
+    console.log("Double-click detected");
   });
-}
 
-window.initMap = initMap;
+  // ##### 1.9
 
-// #### 1.7
+  // ############ 2. Function as a variable ##########
 
-// #### 1.8
-window.addEventListener("dblclick", (e) => {
-  console.log("Double-click detected");
-});
+  // 2.1
+  const greetPeople = () => {
+    console.log("Hello People!");
+  };
 
-// ##### 1.9
+  const sayFirstname = () => {
+    console.log("My first name is Mehmet");
+  };
 
-// ############ 2. Function as a variable ##########
+  const sayLastname = () => {
+    console.log("My last name is Cavdar");
+  };
 
-// 2.1
-const greetPeople = () => {
-  console.log("Hello People!");
-};
+  const funcsArr = [greetPeople, sayFirstname, sayLastname];
 
-const sayFirstname = () => {
-  console.log("My first name is Mehmet");
-};
+  funcsArr.forEach((func) => {
+    func();
+  });
 
-const sayLastname = () => {
-  console.log("My last name is Cavdar");
-};
+  //2.2
 
-const funcsArr = [greetPeople, sayFirstname, sayLastname];
+  function greetings() {
+    console.log("What's up World?");
+  }
 
-funcsArr.forEach((func) => {
-  func();
-});
+  const farewell = () => {
+    console.log("Good bye world!");
+  };
+  greetings();
+  farewell();
 
-//2.2
+  // 2.3
 
-function greetings() {
-  console.log("What's up World?");
-}
+  const functionObj = {
+    question: () => {
+      console.log("How are you");
+    },
+  };
 
-const farewell = () => {
-  console.log("Good bye world!");
-};
-greetings();
-farewell();
+  functionObj.question();
 
-// 2.3
+  // ########## 3. The fastest presser in this realm ##########
 
-const functionObj = {
-  question: () => {
-    console.log("How are you");
-  },
-};
+  const secondsInput = document.querySelector("#seconds");
+  const startBtn = document.querySelector("#startBtn");
 
-functionObj.question();
+  const game = () => {
+    const jsConfetti = new JSConfetti();
+    const draw = document.createElement("h3");
+
+    const confettiFunc = async () => {
+      await jsConfetti.addConfetti({
+        emojis: ["🌈", "⚡️", "💥", "✨", "💫", "🌸", "🎈"],
+        emojiSize: 30,
+        confettiNumber: 200,
+        confettiRadius: 6,
+      });
+    };
+    let waitTime = secondsInput.value * 1000;
+
+    let totalS = 0;
+    let totalL = 0;
+    const gameFunc = (event) => {
+      if (event.key === "s") {
+        totalS++;
+        let textS = document.querySelector("#countS");
+        textS.innerHTML = totalS;
+      }
+      if (event.key === "l") {
+        totalL++;
+        let textS = document.querySelector("#countL");
+        textS.innerHTML = totalL;
+      }
+    };
+
+    if (waitTime !== 0) {
+      window.addEventListener("keydown", gameFunc);
+      const winner = () => {
+        const areaS = document.querySelector("#pressS");
+        const areaL = document.querySelector("pressL");
+        if (totalL > totalS) {
+          document.querySelector("#pressL").style.backgroundColor = "purple";
+          document.querySelector("#pressL").innerHTML = "Congratulations";
+          confettiFunc();
+        } else if (totalL < totalS) {
+          document.querySelector("#pressS").style.backgroundColor = "blue";
+          document.querySelector("#pressS").innerHTML = "Congratulations";
+          confettiFunc();
+        } else if (totalL === 0 && totalS === 0) {
+          document.body.append(
+            (draw.innerHTML = "Do you forget to press the keys?")
+          );
+        } else {
+          document.body.append(
+            (draw.innerHTML = "No one wins! Let's try again?")
+          );
+          document.body.style.backgroundColor = "yellow";
+        }
+        const reStartBtn = document.querySelector("#reStartBtn");
+        reStartBtn.style.display = "flex";
+        startBtn.style.display = "none";
+      };
+      const timer = setTimeout(() => {
+        window.removeEventListener("keydown", gameFunc);
+        winner();
+      }, waitTime);
+    } else {
+      alert("Please set a time!");
+    }
+  };
+
+  startBtn.addEventListener("click", game);
+  reStartBtn.addEventListener("click", game);
+})();
